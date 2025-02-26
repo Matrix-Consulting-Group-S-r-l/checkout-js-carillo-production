7978ea4b805f3c10b3ac6467636f47ea09239d9a
Michele Selvaggio <m.selvaggio@matrixconsulting.it>
2025-02-26 12:48:29 +0100

    pubblico bundle

---
+++ packages/core/src/app/mtxConfig.ts
@@ -0,0 +1,9 @@
+export const mtxConfig = {
+    shippingMethods: {
+        corriereStandard: 'Corriere Espresso (BRT) - Consegna in 4/5 giorni lavorativi',
+        corriereContrassegno: 'Corriere Contrassegno',
+    },
+    OrderConfirmation: {
+        textCod: "Abbiamo ricevuto il tuo ordine. L'ordine è in fase di elaborazione nel nostro sistema. L'elaborazione potrebbe richiedere alcuni minuti.",
+    }
+}
